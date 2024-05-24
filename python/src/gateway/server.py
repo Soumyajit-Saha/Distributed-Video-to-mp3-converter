93db329a915745ca70a3e24a9c984801283f20c4
Soumyajit Saha <57801484+Soumyajit-Saha@users.noreply.github.com>
2024-05-23 23:57:26 -0600

    Update server.py

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -4,16 +4,20 @@ from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
 from storage import util
+from bson.objectid import ObjectId
 
 server = Flask(__name__)
-server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
-mongo = PyMongo(server)
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
 
-fs = gridfs.GridFS(mongo.db)
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
+
 
 @server.route("/login", methods=["POST"])
 def login():
@@ -23,14 +27,15 @@ def login():
         return token
     else:
         return err
-    
+
+
 @server.route("/upload", methods=["POST"])
 def upload():
     access, err = validate.token(request)
 
     if err:
         return err
-    
+
     access = json.loads(access)
 
     if access["admin"]:
@@ -46,7 +51,8 @@ def upload():
         return "success!", 200
     else:
         return "not authorized", 401
-    
+
+
 @server.route("/download", methods=["GET"])
 def download():
     access, err = validate.token(request)
